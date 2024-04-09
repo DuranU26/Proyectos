705ba23406ebbb0d2c1c0fdd2daa2db6cb8ea793
DuranU26 <166355606+DuranU26@users.noreply.github.com>
2024-04-09 00:02:26 -0600

    Add files via upload

---
+++ Practica_7/Practica_7/Pantalla3.swift
@@ -0,0 +1,30 @@
+//
+//  Pantalla3.swift
+//  Practica_7
+//
+//  Created by Ulises Duran on 2/25/23.
+//  Copyright © 2023 Ulises Duran. All rights reserved.
+//
+
+import UIKit
+
+class Pantalla3: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
